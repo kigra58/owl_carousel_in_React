bc6a159da37155e2b753702d7cd93a19b6511eab
kigra58 <kigra58@outlook.com>
2021-09-12 20:26:50 +0530

    first_commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
 import './App.css';
+import './bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+
+<div class="owl-carousel p-5 mt-5 ">
+    <div >
+        {/* <!-- First  --> */}
+         <div class="card  shadow-lg p-3 ">
+            <div class="card-body text-center bg-warning ">
+              <h1 class="text-white"><b> 1 </b> </h1> 
+            </div>
+        </div>
+
+    </div>
+
+    <div>
+        {/* <!-- Second  --> */}
+        <div class="card  shadow-lg p-3  ">
+            <div class="card-body text-center bg-success">
+              <h1 class="text-white"><b>2  </b> </h1> 
+            </div>
+        </div>
+
+    </div>
+
+    <div>
+        {/* <!-- Third   --> */}
+        <div class="card  shadow-lg p-3 ">
+            <div class="card-body text-center bg-secondary ">
+               <h1 class="text-white"><b> 3 </b> </h1>            
+            </div>
+        </div>
+
+    </div>
+
+    <div>
+        {/* <!-- Fourth  --> */}
+        <div class="card  shadow-lg p-3 ">
+            <div class="card-body text-center bg-primary  ">
+               <h1 class="text-white"><b> 4 </b> </h1>            
+            </div>
+        </div>
+
+    </div>
+
+    <div>
+        {/* <!-- Five  --> */}
+        <div class="card p-3 shadow-lg ">
+            <div class="card-body text-center bg-info  ">
+               <h1 class="text-white"><b> 5 </b> </h1>            
+             </div>
+        </div>
+
+    </div>
+
+    <div>
+        {/* <!-- Six  --> */}
+        <div class="card  shadow-lg p-3 ">
+            <div class="card-body text-center bg-danger  ">
+               <h1 class="text-white"><b> 6 </b> </h1>             
+            </div>
+        </div>
+
+    </div>
+
+    <div>
+         {/* Seven   */}
+        <div class="card  shadow-lg p-3 ">
+            <div class="card-body text-center bg-dark ">
+               <h1 class="text-white"><b> 7 </b> </h1>   
+            </div>
+        </div>
+
+    </div>
+
+</div>
+
+
+
     </div>
   );
 }
